9a0a8845001f76ee610eb59385097ce4ba240029
Eyeball Edward <edwardyeung.private@gmail.com>
2015-12-26 22:22:03 +0800

    Amazing imports

--- src/main/java/com/eyeball/randomutils/crafting/CraftingModules.java
+++ src/main/java/com/eyeball/randomutils/crafting/CraftingModules.java
@@ -1,18 +1,8 @@
 package com.eyeball.randomutils.crafting;
 
 import com.eyeball.randomutils.Registry;
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.common.eventhandler.Event;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.gameevent.PlayerEvent;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
-import net.minecraft.inventory.ContainerWorkbench;
-import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.IRecipe;
-import net.minecraft.util.ChatComponentText;
-import net.minecraft.world.World;
 
 public class CraftingModules {
 
